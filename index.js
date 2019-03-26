464b1834a7bd216ccc47d8eb76075732f56e0525
Thomas G <thomas_grefsrud@hotmail.com>
2019-03-26 11:08:43 +0100

    added vue project

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+const L = require('leaflet')
+const axios = require('axios')
+const expressVue = require('express-vue')
+const app = express();
+const port = 8080;
 
+const expressVueMiddleware = expressVue.init();
+app.use(expressVueMiddleware);
+
+const oslo = [59.9139, 10.7522]
+var mymap = L.map('mapid').setView([oslo[0], oslo[1]], 13);
 const access_token = 'pk.eyJ1IjoidmF5cmEiLCJhIjoiY2p0cGhyb2UwMDJ2bzQ0bzc2bmUxYXgwYiJ9.bzvNNOssxSGv4BwarWO9Bw'
+
 
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
@@ -9,3 +19,9 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     id: 'mapbox.streets',
     accessToken: access_token
 }).addTo(mymap);
+
+
+
+app.listen(port, () => {
+    console.log('Listening on port ' + port);
+});
---
+++ weather/src/components/Hello.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="hero">
+    <div>
+      <h1 class="display-3">Hello World</h1>
+      <p class="lead">This is the homepage of your vue app</p>
+      <div id="mapid"></div>
+    </div>
+  </div>
+</template>
+
+<style>
+.hero {
+  height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+.hero .lead {
+  font-weight: 200;
+  font-size: 1.5rem;
+}
+</style>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js PWA'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #35495e;
+}
+
+#mapid {
+  height:500px;
+}
+</style>
