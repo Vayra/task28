2866e9346d7c42dca2940138a79289ca81c43bda
Thomas G <thomas_grefsrud@hotmail.com>
2019-03-26 12:37:17 +0100

    removed eslint

--- weather/src/App.vue
+++ weather/src/App.vue
@@ -1,21 +1,21 @@
 <template>
-  <div id="app">
-    <b-navbar toggleable="md" type="dark" variant="dark">
-      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-      <b-navbar-brand to="/">My Vue App</b-navbar-brand>
-      <b-collapse is-nav id="nav_collapse">
+  <div id='app'>
+    <b-navbar toggleable='md' type='dark' variant='dark'>
+      <b-navbar-toggle target='nav_collapse'></b-navbar-toggle>
+      <b-navbar-brand to='/'>My Vue App</b-navbar-brand>
+      <b-collapse is-nav id='nav_collapse'>
         <b-navbar-nav>
-          <b-nav-item to="/">Home</b-nav-item>
+          <b-nav-item to='/'>Home</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
+    
     <!-- routes will be rendered here -->
-    <router-view />
+    <router-view/>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'app',
   data () {
@@ -24,24 +24,12 @@ export default {
     }
   },
   async created () {
-    await this.refreshActiveUser()
   },
   watch: {
     // everytime a route is changed refresh the activeUser
-    '$route': 'refreshActiveUser'
+    $route: 'refreshActiveUser'
   },
   methods: {
-    login () {
-      this.$auth.loginRedirect()
-    },
-    async refreshActiveUser () {
-      this.activeUser = await this.$auth.getUser()
-    },
-    async logout () {
-      await this.$auth.logout()
-      await this.refreshActiveUser()
-      this.$router.push('/')
-    }
   }
 }
 </script>
@@ -67,7 +55,7 @@ header {
   margin: 0;
   height: 56px;
   padding: 0 16px 0 24px;
-  background-color: #35495E;
+  background-color: #35495e;
   color: #ffffff;
 }
 
@@ -76,7 +64,7 @@ header span {
   position: relative;
   font-size: 20px;
   line-height: 1;
-  letter-spacing: .02em;
+  letter-spacing: 0.02em;
   font-weight: 400;
   box-sizing: border-box;
   padding-top: 16px;
